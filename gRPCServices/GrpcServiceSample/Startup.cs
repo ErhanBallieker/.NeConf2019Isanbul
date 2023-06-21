cb25498168fb4a5ef7b2633b30580787fc1056fe
An author <erhanballieker@hotmail.com>
2023-06-22 00:13:29 +0300

    my commit message

--- gRPCServices/GrpcServiceSample/Startup.cs
+++ gRPCServices/GrpcServiceSample/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +17,7 @@ namespace GrpcServiceSample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddAuthorization(); // Add authorization service
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,14 +30,11 @@ namespace GrpcServiceSample
 
             app.UseRouting();
 
+            app.UseAuthorization(); // Use authorization middleware
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
-
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                });
             });
         }
     }
