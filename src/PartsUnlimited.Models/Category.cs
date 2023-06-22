185acfcd6e17a08f17564a7a52c43fc0d189551c
An author <erhanballieker@hotmail.com>
2023-06-22 12:26:52 +0300

    my commit message

--- src/PartsUnlimited.Models/Category.cs
+++ src/PartsUnlimited.Models/Category.cs
@@ -1,15 +1,13 @@
-   using System.Collections.Generic;
+using System.Collections.Generic;
 
-   namespace PartsUnlimited.Models
-   {
-       public class Category
-       {
-           public int Id { get; set; }
-           public string Name { get; set; }
-           public string Description { get; set; }
-           public string ImageUrl { get; set; }
-           public string Color { get; set; } // New property
-           public List<Product> Products { get; set; }
-       }
-   }
-   
+namespace PartsUnlimited.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; } // Added Color property
+        public List<Product> Products { get; set; }
+    }
+}
